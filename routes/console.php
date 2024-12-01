980443eb6ab814686c554a958d79cb2ce786717c
Artur A. Ribeiro <arturalves.rib+github@gmail.com>
2024-12-01 03:25:48 -0300

    Remoção da notificação periódica de teste

--- routes/console.php
+++ routes/console.php
@@ -10,7 +10,3 @@ Schedule::job(new SetOrderAsPreparing())->everyMinute();
 Schedule::command('queue:work --stop-when-empty')
     ->everyMinute()
     ->withoutOverlapping();
-
-Schedule::call(function () {
-    event(new \App\Events\NotificationSent(2, \App\DTO\Notification::create('Ping', 'Pong')));
-});
